af3baf195f981aa0de35cbab9d74b9b1692e1907
Raul <reudavel@gmail.com>
2021-01-22 00:53:10 -0600

    navbar ubdate

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
 console.log("Hello from webpack");
 import "./index.scss";//modify to use scss files
+
+
+//For changue the navbar style on scroll
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if ( currentScrollPos > 75) {
+    document.getElementById("navbar").style.backgroundColor = "#007EA7";
+  } else {
+    document.getElementById("navbar").style.backgroundColor = "transparent";
+  }
+  prevScrollpos = currentScrollPos;
+}
